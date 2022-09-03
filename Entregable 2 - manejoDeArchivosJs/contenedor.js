ab534865819bd7cd00f82ffb80043381d9a6dbf2
Lautaro <lautarogozalvez@gmail.com>
2022-09-03 03:58:20 -0300

    entregable 2 - manejo de archivos

--- Entregable 2 - manejoDeArchivosJs/contenedor.js
+++ Entregable 2 - manejoDeArchivosJs/contenedor.js
@@ -8,20 +8,19 @@ class Contenedor{
 
     async getData(){
         const data = await fs.promises.readFile(this.file, "utf-8");
-        return data;
+        const parseData = JSON.parse(data);
+        return parseData;
     }
 
     async save(objet){
         try {
-            const data = await fs.promises.readFile(this.file, "utf-8");
-            const parseData = JSON.parse(data);
-
-            objet.id = parseData.length + 1 ;
-            parseData.push(objet);
-
-            await fs.promises.writeFile(this.file, JSON.stringify(parseData, null, 2));
+            const data = await this.getData();
             
-            return objet.id;
+            if(data.length !== 0){
+                await fs.promises.writeFile(this.file, JSON.stringify([...data, {...objet, id: data[data.length - 1].id + 1 }], null, 2), "utf-8");
+            }else{
+                await fs.promises.writeFile(this.file, JSON.stringify([{...objet, id:1}]), "utf-8");
+            }
 
         } catch (error) {
             throw new Error(`hubo un error al intentar guardar el objeto. mensaje: ${error.message}`);
@@ -32,9 +31,8 @@ class Contenedor{
     async getById(id){
         try {
           const data = await this.getData();
-          const parsedData = JSON.parse(data);
     
-          return parsedData.find((producto) => producto.id === id);
+          return data.find(producto => producto.id === id);
         } catch (error) {
             throw new Error(`no se encontro el objeto con id: ${id}. mensaje: ${error.message}`);
         }
@@ -43,21 +41,21 @@ class Contenedor{
     async getAll(){
         const data = await this.getData();
 
-        return JSON.parse(data);
+        return data;
     }
 
     async deleteById(id){
         try {
             
             const data = await this.getData();
-            const parseData = JSON.parse(data);
+            
 
-            const objetIdRemove = parseData.find((product)=>product.id === id);
+            const objetIdRemove = data.find(product => product.id === id);
 
             if(objetIdRemove){
-                const index = parseData.indexOf(objetIdRemove);
-                parseData.splice(index, 1);
-                await fs.promises.writeFile(this.file, JSON.stringify(parseData));
+                const index = data.indexOf(objetIdRemove);
+                data.splice(index, 1);
+                await fs.promises.writeFile(this.file, JSON.stringify(data));
             }else{
                 console.log(`el objeto con id ${id} no existe`);
                 return null;
@@ -72,9 +70,9 @@ class Contenedor{
 
     async deleteAll(){
         try {
-            await fs.promises.writeFile(this.file,JSON.stringify([],null,2))
+            await fs.promises.writeFile(this.file,JSON.stringify([],null,2));
         } catch (error) {
-            return `No se pueden borrar los datos`
+            return `No se pueden borrar los datos`;
         }
     } 
 
--- Entregable 2 - manejoDeArchivosJs/test.js
+++ Entregable 2 - manejoDeArchivosJs/test.js
@@ -1,25 +1,22 @@
 const Contenedor = require("./contenedor");
 
-const contenedor = new Contenedor("products.json");
+const contenedor = new Contenedor("products.txt");
 
 const test = async()=>{
     const id1 = await contenedor.save({title: "goma", price: 50});
     const id2 = await contenedor.save({title: "cartuchera", price: 1599});
     const id3 = await contenedor.save({title: "fibron", price: 350});
     
+    //await contenedor.deleteById(2) // {"title": "cartuchera","price": 1599,"id": 2}
 
-    console.log(id1, id2,id3); //[{"title": "goma","price": 50,"id": 1},{"title": "cartuchera","price": 1599,"id": 2},{"title": "fibron","price": 350,"id": 3}]
-
-    /* await contenedor.deleteById(2) */ // {"title": "cartuchera","price": 1599,"id": 2}
-
-    /* const allObjet = await contenedor.getAll(); //[{"title": "goma","price": 50,"id": 1},{"title": "fibron","price": 350,"id": 3}]
-    console.log(allObjet); */
+    //const allObjet = await contenedor.getAll(); //[{"title": "goma","price": 50,"id": 1},{"title": "fibron","price": 350,"id": 3}]
+    //console.log(allObjet);
  
 
-    /* const Objet = await contenedor.getById(1);
-    console.log(Objet); */ //{"title": "goma","price": 50,"id": 1}
+    //const Objet = await contenedor.getById(1);
+    //console.log(Objet); //{"title": "goma","price": 50,"id": 1}
 
-    /* await contenedor.deleteAll() */ 
+    //await contenedor.deleteAll() 
     
 
 
